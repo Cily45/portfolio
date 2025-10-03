1f0932d21ee1112e015b36c0f42d6318d83bcf4e
cily <cilyana45@gmail.com>
2025-10-03 10:34:53 +0200

    wip

--- component/AppFooter.js
+++ component/AppFooter.js
@@ -2,6 +2,7 @@ class AppFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
 <section class="flex items-center h-12 w-full bg-[#522864] opacity-80 justify-center space-x-2" id="footer">
+<p>Copyright © Cecile Fischer</p>
     <a href="https://www.linkedin.com/in/fischercecile/">Linkedin</a>
     <a href="https://github.com/Cily45">Github</a>
     <a href="https://www.codingame.com/profile/e9311db520e0845cc060d125643ada197720465">Codingame</a>
